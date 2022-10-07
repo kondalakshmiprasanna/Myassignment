955d72def586c8416c190983944cfd90fdc34a64
kondalakshmiprasanna <48619654+kondalakshmiprasanna@users.noreply.github.com>
2022-10-07 18:14:07 -0400

    Add files via upload
    Day1

---
+++ day1/LearnForLoop.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class LearnForLoop {
+
+	public static void main(String[] args) {
+		for (int a=1;a<=10;a++) // initialization, condition, increment
+		{
+		System.out.println(a+"TestLeaf"); //concatination
+		}
+
+	}
+
+}
---
+++ day1/MyFirstClass.java
@@ -0,0 +1,37 @@
+package week1.day1;
+
+public class MyFirstClass {
+// datatype variableName = value;
+/*
+int a=5;
+	byte number1 = 32; // -128 to +128
+	short number2 = -32767;
+	int number3 = 987654321;
+	long number4 = 9876543210L;
+	
+	float number5 = 23425.6426F;
+	double number6 = 98765432345.87654345678;
+	
+	char intial = 'e';
+	boolean hadbreakfast = false;
+	
+	
+	String name = "Test leaf";
+*/
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+		System.out.println(4/2); // quotionent
+		System.out.println(5%2); //  reminder
+		
+		int a = 5;
+		System.out.println(a++); // post increment
+		System.out.println(a); //
+		
+		int b = 5;
+		System.out.println(--b); // pre decreament
+		System.out.println(b);
+		
+	}
+}
